ff0ff930f386bf0536647dbcd03667992416f3a5
dannywang <111034@stu.tnssh.tn.edu.tw>
2020-04-13 21:46:03 +0800

    second commit

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -7,8 +7,8 @@ const ImageCard = ({image}) => {
         <div className='max-w-sm rounded overflow-hidden shadow-lg'>
       <img src={image.webformatURL} className='w-full'/>
       <div className='px-6 py-4'>
-        <div className='font-bold text-purple-500 text-xl mb-2'>
-          Photo by {image.user}
+        <div className='font-bold text-green-500 text-xl mb-2'>
+          Photo by <span className='font-black text-green-700'>{image.user}</span>
         </div>
       <ul>
         <li>
